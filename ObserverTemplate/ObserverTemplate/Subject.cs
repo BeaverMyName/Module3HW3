1b4455ee11072b6317349b33ca8e19cfea13f4f4
Igor Bobro <igor.bobro.lul@gmail.com>
2021-10-13 19:46:06 +0300

    task completed

---
+++ ObserverTemplate/ObserverTemplate/Subject.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ObserverTemplate.Abstractions;
+
+namespace ObserverTemplate
+{
+    public class Subject : ISubject
+    {
+        private EventHandler _eventHandler;
+
+        public void Attach(IObserver observer)
+        {
+            _eventHandler += observer.Update;
+        }
+
+        public void Detach(IObserver observer)
+        {
+            _eventHandler -= observer.Update;
+        }
+
+        public void Notify()
+        {
+            _eventHandler?.Invoke(this, new EventArgs());
+        }
+    }
+}
